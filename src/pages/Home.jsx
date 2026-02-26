0c3a0e6cafb2ad405a1ec1a577e14ff72e82ad42
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-27 02:02:36 +0530

    feat:refactor signup page logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const useNavigateHook = useNavigate();
+    const navigate = useNavigate();
     const token = localStorage.getItem("token");
     useEffect(() => {
         if (token) {
-            console.log("Token found:", token);
+            navigate("/");
         } else {
-            useNavigateHook("/login");
+            navigate("/login");
         }
     }, []);
 

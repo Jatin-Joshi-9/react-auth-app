fb40b56d8c36ac6b6615767b02ceb533e1466d6b
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-01 15:43:59 +0530

    refactor: add the textarea field in InputField

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -4,14 +4,19 @@ interface InputFieldProps {
   label: string;
   name: string;
   type: string;
+  textarea?: boolean;
+  rows?: number;
 }
-const InputField = ({label,name,type}:InputFieldProps) => {
+const InputField = ({label,name,type,textarea=false,rows}:InputFieldProps) => {
+  const as = textarea ? "textarea" : "input";
   return (
     <div className="flex flex-col gap-1">
       <label htmlFor={name} className="label font-bold self-start">{label}</label>
       <Field className="input border-2 border-blue-400 rounded-xl py-1 px-4"
         name={name}
         type={type}
+        as = {as}
+        rows={as === "textarea" ? rows : undefined}
       />
       <ErrorMessage name={name} component="div" className="text-red-500 text-sm" />
     </div>
--- src/pages/CreateTicket.tsx
+++ src/pages/CreateTicket.tsx
@@ -61,7 +61,7 @@ const CreateTicket = () => {
 
           <InputField label="Title" name="title" type="text" />
 
-          <InputField label="Description" name="description" type="text" />
+          <InputField label="Description" name="description" type="text" textarea={true} rows={4} />
 
           <button
             className="bg-blue-200 border-b-black border text-1xl font-bold rounded-3xl my-1.5 py-2 cursor-grab"
